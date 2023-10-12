3b5eabbec3c3316608a001fb23ac9e6b64ae4c9c
naxiner <kirienkodenis2002@gmail.com>
2023-10-13 01:21:19 +0300

    Added Model for Currency

---
+++ Models/Currency.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinView.Models
+{
+    class Currency
+    {
+        public string Id { get; set; } = string.Empty;
+		public int Rank { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+		public string Name { get; set; } = string.Empty;
+		public decimal Supply { get; set; }
+        public decimal MaxSuplly { get; set; }
+        public decimal MarketCapUsd { get; set; }
+        public decimal VolumeUsd24Hr { get; set; }
+        public decimal PriceUsd { get; set; }
+        public decimal ChangePercent24Hr { get; set; }
+        public decimal Vwap24Hr { get; set; }
+        public string Explorer { get; set; } = string.Empty;
+    }
+}
